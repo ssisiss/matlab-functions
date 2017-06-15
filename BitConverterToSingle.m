0ecf2a5de2f5a58d69098f1180d22b03af7ab375
ssisiss <sozq@sina.com>
2017-06-15 22:20:59 +0800

    Create BitConverterToSingle.m

--- BitConverterToSingle.m
+++ BitConverterToSingle.m
@@ -1,10 +1,10 @@
 function  onefloat  = BitConverterToSingle( fourbytes )
-%UNTITLED 此处显示有关此函数的摘要
-%   此处显示详细说明
+%UNTITLED 杩斿洖浠庡瓧鑺傛暟缁勪腑鎸囧畾浣嶇疆鐨勫洓涓瓧鑺傝浆鎹㈡潵鍗曠簿搴︽诞鐐规暟
+%   https://msdn.microsoft.com/zh-cn/library/system.bitconverter.tosingle(v=vs.110).aspx
 onefloat=0.000;
-    fourbytes_11=fourbytes(1:2);%四元数w
+    fourbytes_11=fourbytes(1:2);
     fourbytes_12=fourbytes(3:4);
-    fourbytes_13=fourbytes(5:6);%四元数w
+    fourbytes_13=fourbytes(5:6);
     fourbytes_14=fourbytes(7:8);
     fourbytes_1=myHexstr2num(fourbytes_11);
     fourbytes_2=myHexstr2num(fourbytes_12);
@@ -12,7 +12,7 @@ onefloat=0.000;
     fourbytes_4=myHexstr2num(fourbytes_14);
     
     tem_onefloat=[fourbytes_1 fourbytes_2 fourbytes_3 fourbytes_4];
-    tem_onefloat = uint8(tem_onefloat) ;                      %// cast them to "uint8" if they are not already
+    tem_onefloat = uint8(tem_onefloat) ;          
     onefloat = typecast( tem_onefloat , 'single') ;
 
 end
